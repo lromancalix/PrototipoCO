0eed68dbcdff138cecc56da77b46932b00208891
calix <roman.calix.net@gmail.com>
2020-10-06 15:34:49 -0500

    Se agregan imagenes formateadas

--- src/app/components/pago/pagotarjeta/pagotarjeta.component.ts
+++ src/app/components/pago/pagotarjeta/pagotarjeta.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 import { CarritoService } from '../../../services/carrito/carrito-service.service';
 
 @Component({
@@ -16,11 +18,46 @@ export class PagotarjetaComponent implements OnInit {
   cobroCerrado = new EventEmitter<boolean>();
 
 
-  constructor( public carritoServ: CarritoService ) { }
+  constructor( public carritoServ: CarritoService, private router: Router ) { }
 
   ngOnInit() {
   }
 
 
+  pagar() {
+
+    Swal.fire({
+      allowOutsideClick: false,
+      icon: 'info',
+      title: 'Realizando pago',
+      text: 'Espere un momento por favor..'
+    });
+
+    Swal.showLoading();
+
+    setTimeout(() => {
+      Swal.close();
+      this.agredecimiento();
+    }, 2500);
+
+  }
+
+  agredecimiento() {
+
+    Swal.fire({
+      allowOutsideClick: false,
+      icon: 'success',
+      title: 'Pago realizado exitosamente',
+      showConfirmButton: false
+    });
+
+    setTimeout(() => {
+      this.router.navigate(['/agradecimiento']);
+      Swal.close();
+    }, 2500);
+
+ 
+
+  }
 
 }
